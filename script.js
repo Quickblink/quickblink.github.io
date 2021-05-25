1d78bda02f808767f0a6e0d5ed63e82dd7b39b5e
Eric <eric.koepke@tum.de>
2021-05-25 17:30:54 +0200

    lots of progress

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var copyText = document.getElementById("instruction");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+}
